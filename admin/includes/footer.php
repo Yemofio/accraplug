f33c7b600fb483cdce56691ac212f3db6da60283
Yemofio <95033608+Yemofio@users.noreply.github.com>
2022-12-06 10:55:04 +0000

    Add files via upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,16 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Conatact us: 0558073764</b>
+      </div>
+      <strong>AccraPlug is where you get the best deals.</strong>
+    </div>        
+
+     <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2022 Digney Yemofio</strong>
+    </div> 
+  
+</footer>
